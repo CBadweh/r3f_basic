70a00668800140c7bed397a269846d295262faac
C Badweh <sheenaymudwe@gmail.com>
2025-02-18 17:27:14 -0500

    Add hover and click interaction with the Sphere component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef} from "react"
+import { useRef, useState} from "react"
 import "./App.css";
 
 // React component and props for the Box
 // props: position, side, color
 const Cube = ({ position, side, color }) => {
-  const ref = useRef(); // with the ref, we can access the cube's properties
+  // ANIMATION: // with the ref, we can access the cube's properties to animate our object
+  const ref = useRef(); 
 
   /*
   Animation with React hook useFrame()
-  des: useFrame() run a callback function on every frame
+  des: useFrame() run a callback function on every frame. To access the state of each frame, we need useRef().
   argument: 
      state - contain properites and details about currenet scene
      delta - time in second sine the last frame
@@ -28,6 +29,44 @@ const Cube = ({ position, side, color }) => {
   );
 };
 
+/*
+  INTERACTIVE: Hover and click ability with Sphere object
+  des. 
+    1. When mouse hover the sphere, 
+            change color with color={isHovered ? "lightblue" : "orange"}
+            change speed with ref.current.rotation.y += delta * speed;
+    2. when mouse is clicked,
+            change size with scale={isClicked ? 1.5 : 1}
+*/
+const Sphere = ({ position, args, color }) => {
+  const ref = useRef();
+
+  const [isHovered, setIsHovered] = useState(false); // Change color to light blue when mouse is hover 
+  const [isClicked, setIsClicked] = useState(false);
+
+  useFrame((state, delta, frame) => {
+    const speed = isHovered ? 1 : 0.2;  // INTERACTIVE: Change Speed when hover
+    ref.current.rotation.y += delta * speed;
+  });
+
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))} //INTERACITVE: Change color when hover
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 1}               //INTERACITVE: Change size when click
+    >
+      <sphereGeometry args={args} />
+      <meshStandardMaterial
+        color={isHovered ? "lightblue" : "orange"} // INTERACITVE: Change color when hover
+        wireframe
+      />
+    </mesh>
+  );
+};
+
 
 const App = () => {
   return (
@@ -38,13 +77,16 @@ const App = () => {
       />
 
       {/* Box Mesh include the geometry and the material */}
-      <mesh position= {[1,0,0]} >
+      <mesh position= {[-2,0,0]} >
         <boxGeometry args={[2, 2, 4]} /> 
         <meshStandardMaterial color={"orange"} />  
       </mesh>
 
-      {/* adding Box using React component */}
-      <Cube position={[-1, 0, 0]} color={"green"} args={[1,1,1]} />
+      {/* 1.ADDING Box using React component 2. ANIMATION */}
+      <Cube position={[0, 0, 0]} color={"green"} args={[1,1,1]} />
+
+      {/* INTERACTIVE */}
+      <Sphere position={[3, 0, 0]} size = {[1, 30,30]} color={"orange"}/>
 
     </Canvas>
   );
