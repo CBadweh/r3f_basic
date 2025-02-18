5bec3dbb0b713f0e544eaad96e18462ce704e95b
C Badweh <sheenaymudwe@gmail.com>
2025-02-18 16:18:45 -0500

    Add box and light without using React Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
-
 
 const App = () => {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <Canvas>
+      {/* add lighting */}
+      <directionalLight
+        position={[0, 1, 2]}
+      />
+
+      {/* Box Mesh include the geometry and the material */}
+      <mesh position= {[1,0,0]} >
+        <boxGeometry args={[2, 2, 4]} /> 
+        <meshStandardMaterial color={"orange"} />  
+      </mesh>
+
+    </Canvas>
   );
 };
 
