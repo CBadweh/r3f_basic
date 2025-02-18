3cccf578492af7469e77c6e285626b2a7f25ad1e
C Badweh <sheenaymudwe@gmail.com>
2025-02-18 16:56:43 -0500

    Add 2 animations using useFrame() hook: 1. basic rotation around y axis. 2. rotation based on sine equation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef} from "react"
 import "./App.css";
 
 // React component and props for the Box
 // props: position, side, color
 const Cube = ({ position, side, color }) => {
-  // const ref = useRef();
+  const ref = useRef(); // with the ref, we can access the cube's properties
+
+  /*
+  Animation with React hook useFrame()
+  des: useFrame() run a callback function on every frame
+  argument: 
+     state - contain properites and details about currenet scene
+     delta - time in second sine the last frame
+  */
+  useFrame((state, delta, frame) => {
+    ref.current.rotation.y += delta * 0.2;                              // Basic rotation animation around y axis
+    ref.current.rotation.z += Math.sin(state.clock.elapsedTime) * 0.1;  // Rotation animation based on sine equation on z axis
+    console.log(state);
+  });
 
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
       <boxGeometry args={[side, side, side]} />
       <meshStandardMaterial color={color} />
     </mesh>
